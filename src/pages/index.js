85aaa7319414c7b46bbb79730c1d3a82c82cafa4
laurie <laurie@brandwatch.com>
2020-07-30 10:24:20 +0100

    implement toms homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,28 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Base, Heading, Button, Strong } from "@brandwatch/axiom-components"
+import SideNav from "../components/SideNav"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import "./home.css"
 
 const IndexPage = ({ data }) => {
+  const documentNames = data.allMarkdownRemark.edges.map(edge => {
+    return edge.node.frontmatter.component_name
+  })
+
   return (
-    <Layout>
-      <SEO title="Home" />
-      <h1>Hi Axiom Design Docs</h1>
-      <Link to="/component-render-test/">
-        Have a look at the custom axiom doc components
-      </Link>{" "}
-      <p>
-        They dont have the needed styling and don't match the gallery.io layout
-        blocks yet, but you'll get the idea
-      </p>
-      {data.allMarkdownRemark.edges.map(({ node }) => (
-        <pre>{JSON.stringify(node.frontmatter.component_name)}</pre>
-      ))}
-      {data.allMarkdownRemark.edges.map(({ node }) => (
-        <pre>{JSON.stringify(node.frontmatter)}</pre>
-      ))}
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        <Image />
-      </div>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </Layout>
+    <Base className="home-nav-container ax-theme--day">
+      <SideNav documents={documentNames} />
+      <Base className="home-content">
+        <Button style={{ float: "right", margin: "20px 20px 0 0" }}>
+          Edit this page
+        </Button>
+        <Base className="home-heading">
+          <Heading style={{ "font-size": "80px" }} textSize="display1">
+            <Strong>axiom </Strong>Design Docs
+          </Heading>
+        </Base>
+      </Base>
+    </Base>
   )
 }
 
